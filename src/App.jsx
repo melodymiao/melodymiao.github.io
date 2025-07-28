a6ae1b6562e5b9549c64c2854196a7d1c599f5a8
Melody Miao <melodymiao@berkeley.edu>
2025-07-28 14:48:49 -0700

    removed old projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
-import DolciMangoProject from './pages/DolciMangoProject';
-import MeskiesProject from './pages/MeskiesProject';
 import ChecktProject from './pages/ChecktProject';
-import MoodTunesProject from "./pages/MoodTunesProject";
 import RecWellProject from './pages/RecWellProject';
-import KPGProject from './pages/KPGProject';
 import AugeneProject from './pages/AugeneProject';
 import InnoDProject from './pages/InnoDProject';
 import Fun from './pages/Fun';
@@ -21,23 +17,11 @@ function App() {
         {/* Home Page */}
         <Route path="/" element={<Home />} />
 
-        {/* Dolci Mango */}
-        <Route path="/dolci-mango" element={<DolciMangoProject />} />
-
-        {/* Meskies */}
-        <Route path="/meskies-ethiopia-restaurant" element={<MeskiesProject />} />
-
         {/* Checkt */}
         <Route path="/checkt" element={<ChecktProject />} />
 
-        {/* Moodtunes */}
-        <Route path="/moodtunes" element={<MoodTunesProject />} />
-
         {/* RecWell */}
         <Route path="/recwell" element={<RecWellProject />} />
-
-        {/* KPG */}
-        <Route path="/kpg" element={<KPGProject />} />
 
         {/* Augene */}
         <Route path="/augene" element={<AugeneProject />} />
